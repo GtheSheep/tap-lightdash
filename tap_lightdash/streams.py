5c6391c7b419cfc7e9ac154553da71e482e9dbb7
Gary James <gary.james19@gmail.com>
2023-05-03 14:05:37 +0100

    fix - return type

--- tap_lightdash/streams.py
+++ tap_lightdash/streams.py
@@ -209,7 +209,7 @@ class SavedChartStream(LightdashStream):
         th.Property("views", th.IntegerType)
     ).to_dict()
 
-    def get_records(self, context: Optional[dict]) -> Iterable[dict[str, Any]]:
+    def get_records(self, context: Optional[dict]) -> Iterable[to_dict]:
         """Return a generator of record-type dictionary objects.
 
         Each record emitted should be a dictionary of property names to their values.
